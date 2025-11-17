5cffe34dfe2f37536f0da8f17a2f277a7a659e31
CheriaMehdi <cheriamehdi@gmail.com>
2025-11-17 15:33:24 +0100

    first commit

---
+++ install/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_drive.hpp
@@ -0,0 +1,1 @@
+/home/masters/turtlebot3_ws/src/turtlebot3_simulations/turtlebot3_gazebo/include/turtlebot3_gazebo/turtlebot3_drive.hpp
---
+++ install/turtlebot3_simulations/share/turtlebot3_simulations/local_setup.bash
@@ -0,0 +1,1 @@
+/home/masters/turtlebot3_ws/build/turtlebot3_simulations/ament_cmake_environment_hooks/local_setup.bash
